1011bb258be268e916b3150db7dfbeaa76cfb41f
thomas <sketchbang@gmail.com>
2017-04-23 18:36:12 -0500

    comments

--- config/settings.py
+++ config/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # The Channels project apps
     'channels',
     'channels.delay',
+
+    # Our own app
     'progrock.apps.ProgRockConfig'
 ]
 
