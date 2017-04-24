629074922bc208b685c13179099b6b6d12d08974
thomas <sketchbang@gmail.com>
2017-04-24 18:37:44 -0500

    refactor settings

--- config/settings.py
+++ config/settings.py
@@ -15,18 +15,13 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '@8ld09)zi24rvd0ex*9_jyx#h956_4zyi%a2t&r_tk_!-%0d61'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -46,11 +41,23 @@ INSTALLED_APPS = [
     'progrock.apps.ProgRockConfig'
 ]
 
+# Docker provides the hostname 'redis' for our Redis service
+# to use without Docker, launch Redis locally and use 'localhost'
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "asgi_redis.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("redis", 6379)],
+        },
+        "ROUTING": "config.routing.channel_routing",
+    },
+}
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -98,14 +105,3 @@ LANGUAGE_CODE = 'en-us'
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "asgi_redis.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("redis", 6379)],
-        },
-        "ROUTING": "config.routing.channel_routing",
-    },
-}
-
